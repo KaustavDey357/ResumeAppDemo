e91dff6a3d04e268d86456cacc100307d3b11be4
kaustav <75007246+KaustavDey357@users.noreply.github.com>
2025-10-08 18:15:18 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,23 +1,23 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'screens/resume_screen.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'screens/resume_screen.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('newbox');
   runApp(const ProviderScope(child: MyApp()));
-  await Hive.init();
-  var box = Hive.openBox("newbox");
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Customizable Resume Generator',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      title: 'Resume Demo',
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: const ResumeScreen(),
     );
   }
